1a465a6539aa762469ac95aad782ce6071b34020
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-09-19 18:33:20 -0400

    Update MolecBio.jl

--- src/MolecBio.jl
+++ src/MolecBio.jl
@@ -14,12 +14,23 @@ df = CSV.File(file_path) |> DataFrame
 
 
 function make_output_path()
-    end
+end
 
-function load_table()
-    end
+function load_table(file_path:: string)
+    df = CSV.File(file_path) |> DataFrame
+    return df
+end
 
-function calculate_ddct()
+
+function calculate_ddct(df)
+    
+        df[Symbol("delta_ct"] = df[:target] - df[:normalizer]
+
+        avg_delta_ct_control = df[df[:group .== :control],:delta_ct.mean()
+
+        df["delta_delta_ct"] = df.delta_ct - avg_delta_ct_control
+
+        df["fold_change"] = 2 ** (-df.delta_delta_ct)
     end
 
 function save_table()
