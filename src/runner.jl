b5482a6cba12ef27328d2c51fcaa437c4a4a91ce
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-09-30 20:53:13 -0400

    Update runner.jl

--- src/runner.jl
+++ src/runner.jl
@@ -45,8 +45,8 @@ function main()
     )
     @info "Computing fold change with delta delta ct " first(ddct_table, 5)
     
-    ddct_table = MolecBio.calculate_percent_change(ddct_table)
-    @info "Computing percent change " first(ddct_table, 5)
+    ddct_table = MolecBio.calculate_percent_expression(ddct_table)
+    @info "Computing percent expression " first(ddct_table, 5)
  
     ddct_table_path = MolecBio.make_output_path(args["file_path"], ".csv")
     @info "Saving output table to " ddct_table_path
