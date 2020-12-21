dcf240d0e10f2c52c0627190447a00c82d3ec634
Patrick <21268383+pkmklong@users.noreply.github.com>
2020-12-21 14:16:16 -0800

    Update MolecBio.jl

--- src/MolecBio.jl
+++ src/MolecBio.jl
@@ -28,13 +28,13 @@ function calculate_ddct(df:: DataFrame,
         target:: String,
         normalizer:: String)  
     
-    df[Symbol("delta_ct")] = df[Symbol(target)] .- df[Symbol(normalizer)]
+    df[!, Symbol("delta_ct")] = df[!, Symbol(target)] .- df[!, Symbol(normalizer)]
     
     avg_delta_ct_control = mean(df[df[!, :group] .== control,:delta_ct])
-    df[Symbol("delta_delta_ct")] = df.delta_ct .- avg_delta_ct_control   
+    df[!, Symbol("delta_delta_ct")] = df.delta_ct .- avg_delta_ct_control   
     
     f = (x) -> 2^(-x)
-    df[Symbol("fold_change")] = f.(-df.delta_delta_ct)
+    df[!, Symbol("fold_change")] = f.(-df.delta_delta_ct)
 
     return df
 end
